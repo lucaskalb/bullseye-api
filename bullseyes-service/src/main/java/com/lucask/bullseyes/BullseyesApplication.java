ff46371d298ebe557a565b76dd35b2ce4c6f9067
Lucas Kalb <lucas.kalb@live.com>
2017-07-15 01:24:54 -0300

    Initial project

---
+++ bullseyes-service/src/main/java/com/lucask/bullseyes/BullseyesApplication.java
@@ -0,0 +1,12 @@
+package com.lucask.bullseyes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullseyesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullseyesApplication.class, args);
+	}
+}
---
+++ bullseyes-service/src/test/java/com/lucask/bullseyes/BullseyesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lucask.bullseyes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BullseyesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
